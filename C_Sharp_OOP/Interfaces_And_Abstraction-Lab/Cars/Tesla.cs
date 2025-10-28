a35f2a67c7dbc14df75c24299679346ed49b1977
Yusein Bankeli <Bankeli995@gmail.com>
2025-10-28 21:01:37 +0200

    Abstraction and Inferfaces

---
+++ C_Sharp_OOP/Interfaces_And_Abstraction-Lab/Cars/Tesla.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cars
+{
+    public class Tesla : Car, IElectricCar
+    {
+        public Tesla(string model, string color, int battery) 
+            : base(model, color)
+        {
+            this.Battery = battery;
+        }
+
+        public int Battery { get; private set; }
+
+        public override string ToString()
+        {
+            return base.ToString() + $"with {this.Battery} Batteries";
+        }
+    }
+}
