46184989f441dd002d5c6f3e8b55b1cb9ea290f4
Yusein Bankeli <Bankeli995@gmail.com>
2025-10-29 20:02:58 +0200

    Encapsulation-Exercises

---
+++ C_Sharp_OOP/Encapsulation - Exercises/ShoppingSpree/Person.cs
@@ -0,0 +1,51 @@
+﻿namespace ShoppingSpree
+{
+    public class Person
+    {
+        private string name;
+        private decimal money;
+       //private List<Product> bag;
+
+        public Person(string name, decimal money)
+        {
+            Name = name;
+            Money = money;
+            Bag = new List<Product>();
+        }
+        public List<Product> Bag { get; set; }
+        public string Name
+        {
+            get { return name; }
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                    throw new ArgumentException("Name cannot be empty");
+                name = value;
+            }
+        }
+
+        public decimal Money
+        {
+            get { return money; }
+            private set
+            {
+                if (value < 0)
+                    throw new ArgumentException("Money cannot be negative");
+                money = value;
+            }
+        }
+
+        public void BuyProduct(Product product)
+        {
+            if (product.Cost > Money)
+            {
+                Console.WriteLine($"{Name} can't afford {product.Name}");
+                return;
+            }
+
+            Money -= product.Cost;
+            Bag.Add(product);
+            Console.WriteLine($"{Name} bought {product.Name}");
+        }
+    }
+}
---
+++ C_Sharp_OOP/Encapsulation - Exercises/ShoppingSpree/Program.cs
@@ -0,0 +1,72 @@
+﻿namespace ShoppingSpree
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+			try
+			{
+                var peopleInput = Console.ReadLine().Split(";");
+                var people = new List<Person>();
+
+                foreach (var personInfo in peopleInput)
+                {
+                    var tokens = personInfo.Split('=');
+                    string name = tokens[0];
+                    decimal money = decimal.Parse(tokens[1]);
+
+                    people.Add(new Person(name, money));
+                }
+
+
+                var productInput = Console.ReadLine().Split(";", StringSplitOptions.RemoveEmptyEntries);
+                var products = new List<Product>();
+
+                foreach (var productInfo in productInput)
+                {
+                    var tokens = productInfo.Split('=');
+                    string name = tokens[0];
+                    decimal cost = decimal.Parse(tokens[1]);
+
+                    products.Add(new Product(name, cost));
+                }
+
+                string input = string.Empty;
+
+                while ((input = Console.ReadLine()) != "END")
+                {
+                    string[] tokens = input.Split();
+                    string personName = tokens[0];
+                    string productName = tokens[1];
+
+                    Person person = people.FirstOrDefault(p => p.Name == personName);
+                    Product product = products.FirstOrDefault(p => p.Name == productName);
+
+                    if (product == null || person == null) continue;
+
+                    if (person.Money >= product.Cost)
+                    {
+                        person.BuyProduct(product);
+                    }
+                    else
+                        Console.WriteLine($"{person.Name} can't afford {product.Name}");
+
+                }
+
+                foreach (var persona in people)
+                {
+                    string boughtProducts = persona.Bag.Count > 0
+                        ? string.Join(", ", persona.Bag.Select(p => p.Name))
+                        : "Nothing bought";
+
+                    Console.WriteLine($"{persona.Name} - {boughtProducts}");
+                }
+            }
+			catch (ArgumentException ex)
+			{
+
+                Console.WriteLine(ex.Message);
+			}
+        }
+    }
+}
