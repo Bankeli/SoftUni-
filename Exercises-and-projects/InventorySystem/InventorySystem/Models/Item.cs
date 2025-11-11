cf25c14856e6dce2a5ee5a8a1fc0f2f49bcd51d3
Yusein Bankeli <Bankeli995@gmail.com>
2025-11-11 22:06:02 +0200

    InventorySystem

---
+++ Exercises-and-projects/InventorySystem/InventorySystem/Models/Item.cs
@@ -0,0 +1,25 @@
+﻿using InventorySystem.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventorySystem.Models
+{
+    internal abstract class Item : IItem
+    {
+        protected Item(string name, double weight)
+        {
+            Name = name;
+            Weight = weight;
+        }
+
+        public string Name { get; private set; }
+
+        public double Weight { get; private set; }
+
+        public virtual string GetInfo() => $"Item: {Name} (Weight: {Weight}kg)";
+
+    }
+}
